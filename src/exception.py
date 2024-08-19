74f19023f1ade38cf0ed30741142e09865ff50d1
Sahil Faizal <sahilfaizal0704@gmail.com>
2024-08-19 07:34:59 -0700

    project structure, logging and exceptions

--- src/exception.py
+++ src/exception.py
@@ -1,4 +1,6 @@
 import sys 
+import logging 
+
 
 def error_message_details(error, error_detail:sys):
     _, _, exec_tb = error_detail.exc_info()
@@ -13,8 +15,15 @@ def error_message_details(error, error_detail:sys):
 
 class CustomException(Exception):
     def __init__(self, error_message, error_details:sys):
-        super.__init__(error_message)
+        super().__init__(error_message)
         self.error_message = error_message_details(error_message, error_detail=error_details)
     
     def __str__(self):
         return self.error_message
+
+# if __name__=="__main__":
+#     try:
+#         a = 1/0
+#     except Exception as e:
+#         logging.info("Divide by Zero")
+#         raise CustomException(e,sys)
